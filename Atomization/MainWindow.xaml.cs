dd9562c683c4e8be526a9376d36a875db55db597
Yishan Zhao <yishanzh@umd.edu>
2019-05-25 20:52:13 -0400

    Refactored Initialization

--- Atomization/MainWindow.xaml.cs
+++ Atomization/MainWindow.xaml.cs
@@ -35,27 +35,45 @@ namespace Atomization
 			#region Economy initialization
 			Economy.Content = (int)Data.Me.Economy.Value.Value;
 			Data.Me.Economy.Value.OnValueChanged += (n, pv, nv) => Economy.Content = (int)nv;
-
 			#endregion
 
+			#region High Education Population Initialization
 			HiEduPopu.Content = (int)Data.Me.HiEduPopu.Value.Value;
 			Data.Me.HiEduPopu.Value.OnValueChanged += (n, pv, nv) => HiEduPopu.Content = (int)nv;
 
+			#endregion
+
+			#region Army Initialization
 			Army.Content = (int)Data.Me.Army.Value.Value;
 			Data.Me.Army.Value.OnValueChanged += (n, pv, nv) => Army.Content = (int)nv;
 
+			#endregion
+
+			#region Navy Initialization
 			Navy.Content = (int)Data.Me.Navy.Value.Value;
 			Data.Me.Navy.Value.OnValueChanged += (n, pv, nv) => Navy.Content = (int)nv;
 
+			#endregion
+
+			#region Food Initialization
 			Food.Content = (int)Data.Me.Food.Value.Value;
 			Data.Me.Food.Value.OnValueChanged += (n, pv, nv) => Food.Content = (int)nv;
 
+			#endregion
+
+			#region Raw Material Initialization
 			RawMaterial.Content = (int)Data.Me.Economy.Value.Value;
 			Data.Me.RawMaterial.Value.OnValueChanged += (n, pv, nv) => RawMaterial.Content = (int)nv;
 
+			#endregion
+
+			#region Nuclear Initialization
 			NuclearMaterial.Content = (int)Data.Me.NuclearMaterial.Value.Value;
 			Data.Me.NuclearMaterial.Value.OnValueChanged += (n, pv, nv) => NuclearMaterial.Content = (int)nv;
 
+			#endregion
+
+			#region Stability Initialization
 			ProgBar_Stability.Value = Data.Me.Stability.Value.Value;
 			Stability.Content = (int)Data.Me.Stability.Value.Value;
 			Data.Me.Stability.Value.OnValueChanged += (n, pv, nv) =>
@@ -64,6 +82,7 @@ namespace Atomization
 				ProgBar_Stability.Value = nv;
 			};
 
+			#endregion
 		}
 
 		#region Menu item actions
