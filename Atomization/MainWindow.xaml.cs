49cad6f6d762f97757def679508833f46bde03f9
Yishan Zhao <yishanzh@umd.edu>
2020-02-09 22:07:32 -0500

    preparing to remove deprecated code

--- Atomization/MainWindow.xaml.cs
+++ Atomization/MainWindow.xaml.cs
@@ -21,54 +21,54 @@ namespace Atomization
 
 			DataContext = this;
 
-			#region Economy initialization
-			Economy.DataContext = Data.Me.Economy;
-			EconomyGrowth.ItemsSource = Data.Me.Economy.Growth.Items;
-			EconomyGrowth_Sum.DataContext = Data.Me.Economy.Growth;
-			#endregion
+			//#region Economy initialization
+			//Economy.DataContext = Data.Me.Economy;
+			//EconomyGrowth.ItemsSource = Data.Me.Economy.Growth.Items;
+			//EconomyGrowth_Sum.DataContext = Data.Me.Economy.Growth;
+			//#endregion
 
-			#region High Education Population Initialization
-			HiEduPopu.DataContext = Data.Me.Population;
-			HiEduGrowth.ItemsSource = Data.Me.Population.Growth.Items;
-			HiEduGrowth_Sum.DataContext = Data.Me.Population.Growth;
-			#endregion
+			//#region High Education Population Initialization
+			//HiEduPopu.DataContext = Data.Me.Population;
+			//HiEduGrowth.ItemsSource = Data.Me.Population.Growth.Items;
+			//HiEduGrowth_Sum.DataContext = Data.Me.Population.Growth;
+			//#endregion
 
-			#region Army Initialization
-			Army.DataContext = Data.Me.Army;
-			ArmyGrowth.ItemsSource = Data.Me.Army.Growth.Items;
-			ArmyGrowth_Sum.DataContext = Data.Me.Army.Growth;
-			#endregion
+			//#region Army Initialization
+			//Army.DataContext = Data.Me.Army;
+			//ArmyGrowth.ItemsSource = Data.Me.Army.Growth.Items;
+			//ArmyGrowth_Sum.DataContext = Data.Me.Army.Growth;
+			//#endregion
 
-			#region Navy Initialization
-			Navy.DataContext = Data.Me.Navy;
-			NavyGrowth.ItemsSource = Data.Me.Navy.Growth.Items;
-			NavyGrowth_Sum.DataContext = Data.Me.Navy.Growth;
-			#endregion
+			//#region Navy Initialization
+			//Navy.DataContext = Data.Me.Navy;
+			//NavyGrowth.ItemsSource = Data.Me.Navy.Growth.Items;
+			//NavyGrowth_Sum.DataContext = Data.Me.Navy.Growth;
+			//#endregion
 
-			#region Food Initialization
-			Food.DataContext = Data.Me.Food;
-			FoodGrowth.ItemsSource = Data.Me.Food.Growth.Items;
-			FoodGrowth_SUm.DataContext = Data.Me.Food.Growth;
-			#endregion
+			//#region Food Initialization
+			//Food.DataContext = Data.Me.Food;
+			//FoodGrowth.ItemsSource = Data.Me.Food.Growth.Items;
+			//FoodGrowth_SUm.DataContext = Data.Me.Food.Growth;
+			//#endregion
 
-			#region Raw Material Initialization
-			RawMaterial.DataContext = Data.Me.RawMaterial;
-			RawMaterialGrowth.ItemsSource = Data.Me.RawMaterial.Growth.Items;
-			RawMaterialGrowth_Sum.DataContext = Data.Me.RawMaterial.Growth;
-			#endregion
+			//#region Raw Material Initialization
+			//RawMaterial.DataContext = Data.Me.RawMaterial;
+			//RawMaterialGrowth.ItemsSource = Data.Me.RawMaterial.Growth.Items;
+			//RawMaterialGrowth_Sum.DataContext = Data.Me.RawMaterial.Growth;
+			//#endregion
 
-			#region Nuclear Material Initialization
-			NuclearMaterial.DataContext = Data.Me.NuclearMaterial;
-			NuclearMaterialGrowth.ItemsSource = Data.Me.NuclearMaterial.Growth.Items;
-			NuclearMaterialGrowth_Sum.DataContext = Data.Me.NuclearMaterial.Growth;
-			#endregion
+			//#region Nuclear Material Initialization
+			//NuclearMaterial.DataContext = Data.Me.NuclearMaterial;
+			//NuclearMaterialGrowth.ItemsSource = Data.Me.NuclearMaterial.Growth.Items;
+			//NuclearMaterialGrowth_Sum.DataContext = Data.Me.NuclearMaterial.Growth;
+			//#endregion
 
-			#region Stability Initialization
-			ProgBar_Stability.DataContext = Data.Me.Stability;
-			Stability.DataContext = Data.Me.Stability;
-			StabilityGrowth.ItemsSource = Data.Me.Stability.Growth.Items;
-			StabilityGrowth_Sum.DataContext = Data.Me.Stability.Growth;
-			#endregion
+			//#region Stability Initialization
+			//ProgBar_Stability.DataContext = Data.Me.Stability;
+			//Stability.DataContext = Data.Me.Stability;
+			//StabilityGrowth.ItemsSource = Data.Me.Stability.Growth.Items;
+			//StabilityGrowth_Sum.DataContext = Data.Me.Stability.Growth;
+			//#endregion
 		}
 
 		#region Menu item actions
