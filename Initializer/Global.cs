a441c5dc7b218c893d185065970be6b8e54f2dc6
Yishan Zhao <yishanzh@umd.edu>
2020-03-25 11:58:42 -0400

    adding new configuration file

--- Initializer/Global.cs
+++ Initializer/Global.cs
@@ -1,10 +1,4 @@
-﻿using LCGuidebook.Core;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Text;
-
-namespace LCGuidebook.Initializer
+﻿namespace LCGuidebook.Initializer
 {
 	/// <summary>
 	///		This class is designed for exchanging data between scripts and domain model
