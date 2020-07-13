7ff647a87be8824902275b3b54652fe3e5213abc
Yishan Zhao <yishanzh@umd.edu>
2020-07-13 18:13:48 -0400

    modifying data structures

---
+++ Initializer/LoadFigures.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace LCGuidebook.Initializer.Manager
+{
+	public static partial class Loader
+	{
+		private static Dictionary<string, string> _FigureNamesMap = new Dictionary<string, string>();
+
+		public static void InitializeFigures()
+		{
+			LoadFigureNamesMap(ToXmlDoc(GeneratePath("config", "value_definitions", "figures_definition.initcfg")));
+		}
+
+		private static void LoadFigureNamesMap(XmlNodeList doc)
+		{
+			foreach (XmlNode node in doc)
+			{
+				var name = node.Attributes["name"].InnerText;
+				var id = node.Attributes["identifier"]?.InnerText ?? name;
+				_FigureNamesMap.Add(id, name);
+			}
+		}
+
+	}
+}
